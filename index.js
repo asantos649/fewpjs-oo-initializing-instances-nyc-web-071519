fc13a23a42469f1417ab06fe237d17f5fc0bb56b
Andrew Santos <asantos@alumni.nd.edu>
2019-09-05 17:13:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here
+function Breakfast (food, drink) {
+        this.food = food
+        this.drink = drink
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+  class Dinner {
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      this._dessert = dessert;
+    }
+  }
